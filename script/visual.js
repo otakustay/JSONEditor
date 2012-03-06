03c0622baf9d170f72cbdd7b53b6cb7a80575967
otakustay <otakustay@gmail.com>
2012-03-06 21:15:02 +0800

    Refactor type determine logic, now accepts type extension system using registerTypeExtension function

--- script/visual.js
+++ script/visual.js
@@ -47,82 +47,118 @@
     }
 
     function generatePropertySection(key, value, container) {
-        var type = getType(value);
-        var wrapper = createElement('div', 'property ' + type);
+        var type = getTypeConfig(value);
+        var className = 'type-' + type.name;
+        if (type.baseType) {
+            className += ' type-' + type.baseType;
+        }
+        var wrapper = createElement('div', 'property ' + className);
 
         var keyElement = createElement('span', 'key', key);
         wrapper.appendChild(keyElement);
 
         var valueElement = createElement('div', 'value');
-        blockGenerator[type](value, valueElement);
+        type.render(value, valueElement);
         wrapper.appendChild(valueElement);
 
         container.appendChild(wrapper);
     }
 
-    var blockGenerator = {
-        simple: function(value, container) {
-            var span = createElement('span', '', value)
+
+    // 不同类型键/值的渲染
+    var types = {};
+    (function() {
+        function simple(value, container) {
+            var span = createElement('span', '', value + '')
             container.appendChild(span);
-        },
+        };
 
-        null: function(value, container) {
-            blockGenerator.simple('null', container);
-        },
-
-        undefined: function(value, container) {
-            blockGenerator.simple('undefined', container);
-        },
-
-        boolean: function(value, container) {
-            blockGenerator.simple(value, container);
-        },
-
-        number: function(value, container) {
-            blockGenerator.simple(value, container);
-        },
-
-        string: function(value, container) {
-            blockGenerator.simple('"' + value + '"', container);
-        },
-
-        array: function(array, container) {
-            var start = createElement('span', 'array-start', '[');
-            container.appendChild(start);
-
-            var content = createElement('div', 'array-content');
-            for (var i = 0; i < array.length; i++) {
-                var item = array[i];
-                var type = getType(item);
-                var wrapper = createElement('div', 'value ' + type, '');
-                blockGenerator[type](item, wrapper);
-                content.appendChild(wrapper);
-            }
-            container.appendChild(content);
-
-            var end = createElement('span', 'array-end', ']');
-            container.appendChild(end);
-        },
-
-        object: function(o, container) {
-            var start = createElement('span', 'object-start', '{');
-            container.appendChild(start);
-
-            var content = createElement('div', 'object-content', '');
-            for (var key in o) {
-                var value = o[key];
-                generatePropertySection(key, value, content);
-            }
-            container.appendChild(content);
-
-            var end = createElement('span', 'object-end', '}');
-            container.appendChild(end);
+        var simpleTypes = ['null', 'undefined', 'boolean', 'number'];
+        for (var i = 0; i < simpleTypes.length; i++) {
+            var type = simpleTypes[i];
+            types[type] = {
+                name: type,
+                render: simple,
+                extensions: []
+            };
         }
-    };
+        types['string'] = {
+            name: 'string',
+            render: function(value, container) {
+                simple('"' + value + '"', container);
+            },
+            extensions: []
+        };
+        types['array'] = {
+            name: 'array',
+            render: function(array, container) {
+                var start = createElement('span', 'array-start', '[');
+                container.appendChild(start);
 
-    function getType(o) {
-        return Object.prototype.toString.call(o).slice(8, -1).toLowerCase();
+                var content = createElement('div', 'array-content');
+                for (var i = 0; i < array.length; i++) {
+                    var item = array[i];
+                    var type = getTypeConfig(item);
+                    var className = 'valuetype-' + type.name;
+                    if (type.baseType) {
+                        className += ' type-' + type.baseType;
+                    }
+                    var wrapper = createElement('div', 'value ' + className, '');
+                    type.render(item, wrapper);
+                    content.appendChild(wrapper);
+                }
+                container.appendChild(content);
+
+                var end = createElement('span', 'array-end', ']');
+                container.appendChild(end);
+            },
+            extensions: []
+        };
+        types['object'] = {
+            name: 'object',
+            render: function(o, container) {
+                var start = createElement('span', 'object-start', '{');
+                container.appendChild(start);
+
+                var content = createElement('div', 'object-content', '');
+                for (var key in o) {
+                    var value = o[key];
+                    generatePropertySection(key, value, content);
+                }
+                container.appendChild(content);
+
+                var end = createElement('span', 'object-end', '}');
+                container.appendChild(end);
+            },
+            extensions: []
+        };
+    }());
+
+    function getTypeConfig(o) {
+        var baseType = {}.toString.call(o).slice(8, -1).toLowerCase();
+        var baseTypeConfig = types[baseType];
+        for (var i = 0; i < baseTypeConfig.extensions.length; i++) {
+            var extension = baseTypeConfig.extensions[i];
+            var isMatch = typeof extension.matches === 'function' ? extension.matches(o) : extension.matches.test(o);
+            if (isMatch) {
+                baseTypeConfig = extension;
+                break;
+            }
+        }
+
+        return baseTypeConfig;
     }
+
+    function registerTypeExtension(typeName, baseType, matches, render) {
+        var config = {
+            name: typeName,
+            baseType: baseType,
+            matches: matches || function() { return false; },
+            render: render || types[baseType].render
+        };
+        types[baseType].extensions.push(config);
+    }
+    window.registerTypeExtension = registerTypeExtension;
 }());
 
 $(document).on(
