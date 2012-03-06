211d9fbb987b0d7ffe7c118e5ab1e08c2639a3dd
otakustay <otakustay@gmail.com>
2012-03-06 21:44:07 +0800

    Add a date-editor plugin as a demo of type extension system

---
+++ plugins/date-editor/plugin.js
@@ -0,0 +1,33 @@
+(function() {
+    var regex = /^\/Date\(\d+\+\d{4}\)\/$/;
+
+    function pad(n) {
+        return n < 10 ? '0' + n : n;
+    }
+
+    function render(value, container) {
+        var ticks = parseInt(value.substring(6), 10);
+        var date = new Date(ticks);
+        var str = date.getFullYear() + '-' + 
+            pad(date.getMonth() + 1) + '-' + 
+            pad(date.getDate()) + ' ' + 
+            pad(date.getHours()) + ':' + 
+            pad(date.getMinutes()) + ':' + 
+            pad(date.getSeconds());
+        if (date.getMilliseconds()) {
+            str += '.' + date.getMilliseconds();
+        }
+
+        var span = document.createElement('span');
+        span.appendChild(document.createTextNode(value));
+
+        var hint = document.createElement('small');
+        hint.className = 'date-hint';
+        hint.appendChild(document.createTextNode(str));
+
+        container.appendChild(span);
+        container.appendChild(hint);
+    }
+
+    registerTypeExtension('date', 'string', regex, render);
+}());
