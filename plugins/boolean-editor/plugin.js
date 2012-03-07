618a5249a3916f90c0395b1e25d209a23de8077e
otakustay <otakustay@gmail.com>
2012-03-07 23:37:50 +0800

    Modify boolean-editor plugin to user Accesor component

--- plugins/boolean-editor/plugin.js
+++ plugins/boolean-editor/plugin.js
@@ -5,23 +5,12 @@
             '<span class="false">false</span>' + 
         '</div>';
 
-    function setValue(value, element) {
-        element.innerHTML = value;
-
-        var popup = requestAttachedPopupFor(element);
-        if (popup) {
-            var current = $(popup.dom).find('.' + value);
-            current.siblings().removeClass('current');
-            current.addClass('current');
-        }
-    }
-
     function createEditor(e) {
         var dom = $(e.popup.dom);
         var target = e.domEvent.target;
-        var currentValue = target.textContent.trim();
+        var accessor = e.domEvent.accessor;
         dom.html(editorTemplate);
-        dom.find('.' + currentValue).addClass('current');
+        dom.find('.' + accessor.value).addClass('current');
 
         // 选择true/false值
         dom.on(
@@ -30,7 +19,13 @@
             function(e) {
                 var value = e.target.innerHTML.trim() === 'true' ? true : false;
 
-                setValue(value, target);
+                accessor.value = value;
+                var popup = requestAttachedPopupFor(target);
+                if (popup) {
+                    var current = $(popup.dom).find('.' + value);
+                    current.siblings().removeClass('current');
+                    current.addClass('current');
+                }
             }
         );
     }
