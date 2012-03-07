d8b73eb037fae293e6822717434c807936af0505
otakustay <otakustay@gmail.com>
2012-03-08 03:48:26 +0800

    Fix bug

--- plugins/boolean-editor/plugin.js
+++ plugins/boolean-editor/plugin.js
@@ -13,7 +13,7 @@
         dom.find('.' + accessor.value).addClass('current');
 
         // 选择true/false值
-        dom.on(
+        dom.firstElementChild.on(
             'click',
             '.true, .false',
             function(e) {
