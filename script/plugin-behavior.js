87e6e96a2896a5d6ddeb2fa4c347d7d354ff2250
otakustay <otakustay@gmail.com>
2012-03-08 03:48:43 +0800

    Add slide behavior

--- script/plugin-behavior.js
+++ script/plugin-behavior.js
@@ -78,3 +78,106 @@
         return instance;
     };
 }());
+
+// Slide
+(function() {
+    var tolerance = 10;
+    
+    function computeAngle(x, y) {
+        // 第一区间
+        if (x >= 0 && y >= 0) {
+            return Math.atan(x / y);
+        }
+        // 第二区间
+        else if (x >=0 && y <= 0) {
+            return Math.PI / 2 + Math.atan(-y / x);
+        }
+        // 第三区间
+        else if (x <= 0 && y <= 0) {
+            return Math.PI + Math.atan(-x / -y);
+        }
+        // 第四区间
+        else {
+            return Math.PI + Math.PI / 2 + Math.atan(y / -x);
+        }
+    }
+
+    behavior.slide = function(directionCount, directionStartAngle) {
+        directionCount = directionCount || 0;
+        directionStartAngle = directionStartAngle || 0;
+
+        function createEvent(type, domEvent, validTarget, originX, originY, agent, accessor) {
+            var currentX = domEvent.clientX;
+            var currentY = domEvent.clientY;
+            var offsetX = currentX - originX;
+            var offsetY = currentY - originY;
+            var angle = computeAngle(offsetX, -offsetY);
+
+            var e = {
+                type: type,
+                domEvent: domEvent,
+                target: validTarget,
+                originX: originX,
+                originY: originY,
+                currentX: currentX,
+                currentY: currentY,
+                offsetX: offsetX,
+                offsetY: offsetY,
+                angle: angle,
+                agent: agent,
+                accessor: accessor
+            };
+            if (directionCount > 0) {
+                var perDirectionAngle = Math.PI * 2 / directionCount;
+                e.direction = Math.ceil((angle - directionStartAngle) / perDirectionAngle) % (directionCount + 1);
+            }
+
+            return e;
+        }
+
+        function startSlide(e) {
+            var slider = this;
+            var validTarget = e.target;
+            var originX = e.clientX;
+            var originY = e.clientY;
+            var previousDirection = -1;
+            var agent = e.agent;
+            var accessor = e.accessor;
+
+            var startEvent = createEvent('start', e, validTarget, originX, originY, agent, accessor);
+            this.trigger(startEvent);
+
+            if (startEvent.cancel) {
+                return;
+            }
+
+            function move(e) {
+                var moveEvent = createEvent('move', e, validTarget, originX, originY);
+                slider.trigger(moveEvent);
+
+                // TODO: 追查为何direction会是负数
+                if (moveEvent.direction > 0 && moveEvent.direction !== previousDirection) {
+                    previousDirection = moveEvent.direction;
+                    var directionChangeEvent = createEvent('directionchange', e, validTarget, originX, originY, agent, accessor);
+                    slider.trigger(directionChangeEvent);
+                }
+            }
+
+            function release(e) {
+                var endEvent = createEvent('end', e, validTarget, originX, originY, agent, accessor);
+                slider.trigger(endEvent);
+
+                $.document.off('mousemove', move).off('mouseup', release);
+            }
+
+            $.document.on('mousemove', move).on('mouseup', release);
+        }
+
+        var instance = behavior.createNew('slide');
+        instance.enable = function() {
+            this.agent.on('mousedown', startSlide.bind(this));
+        }
+
+        return instance;
+    }
+}());
