f2027ceebfb082a2c9d3a615beaaca3fb5f66dde
otakustay <otakustay@gmail.com>
2012-03-17 21:48:50 +0800

    Enable slide behavior for boolean editor, slide left or right to change value

--- plugins/boolean-editor/plugin.js
+++ plugins/boolean-editor/plugin.js
@@ -30,8 +30,47 @@
         );
     }
 
+
+    function startSlide(e) {
+        var originalValue = e.accessor.value;
+        // 如果鼠标在20px范围内，不设置值，将原值高亮，否则向左是false，向右是true
+        var directionMapping = [originalValue, 'false', 'true'];
+
+        function markValue(e) {
+            var popup = requestAttachedPopupFor(e.target);
+            var accessor = e.accessor;
+            if (popup) {
+                var container = popup.dom.firstElementChild;
+                $(container).children().removeClass('current');
+
+                var value = directionMapping[e.direction];
+                container.querySelector('.' + value).classList.add('current');
+            }
+        }
+
+        function applyValue(e) {
+            var popup = requestAttachedPopupFor(e.target);
+            var accessor = e.accessor;
+            if (popup) {
+                var text = popup.dom.firstElementChild.querySelector('.current').textContent.trim();
+                accessor.value = text === 'true' ? true : false;
+            }
+
+            this.off('directionchange');
+            this.off('end');
+        }
+
+        this.on('directionchange', markValue);
+        this.on('end', applyValue);
+    }
+
     var agent = getAgentFor('value').ofType('boolean');
+
     var popup = behavior.popup();
     popup.on('fill', createEditor);
     agent.addBehavior(popup);
+
+    var slide = behavior.slide(2, 0, 20); // 分左右2个方向
+    slide.on('start', startSlide);
+    agent.addBehavior(slide);
 }());
